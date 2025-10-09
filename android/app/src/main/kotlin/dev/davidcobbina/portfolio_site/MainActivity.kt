c648fa08e08dbf88480041d06ce9640e1c72e13d
Anandhu <anandhu1407@gmail.com>
2025-10-09 22:18:52 +0530

    start

--- android/app/src/main/kotlin/dev/davidcobbina/portfolio_site/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package dev.anandhu.portfolio_site
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/dev/davidcobbina/portfoliosite/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package dev.anandhu.portfoliosite
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
